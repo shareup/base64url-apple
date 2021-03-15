c18314789bb3623b2cce9e50b9ca272ea0db9640
Anthony Drendel <me@anthonydrendel.com>
2021-03-15 19:38:37 +0100

    Ensure Base64 URL Encoded strings can be turned back into UUIDs

--- Tests/Base64URLTests/Base64URLTests.swift
+++ Tests/Base64URLTests/Base64URLTests.swift
@@ -46,5 +46,16 @@ final class Base64Tests: XCTestCase {
 
         XCTAssertEqual("Z/Jmr6iCSUm39lLh8+3AjQ==", data.base64EncodedString())
         XCTAssertEqual("Z_Jmr6iCSUm39lLh8-3AjQ", data.base64URLEncodedString())
+
+        let decoded = try XCTUnwrap(Data(base64URLEncoded: "Z_Jmr6iCSUm39lLh8-3AjQ"))
+        var bytes: uuid_t = (0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+        decoded.withUnsafeBytes { (ptr: UnsafeRawBufferPointer) -> Void in
+            bytes.0 = ptr[0]; bytes.1 = ptr[1]; bytes.2 = ptr[2]; bytes.3 = ptr[3]
+            bytes.4 = ptr[4]; bytes.5 = ptr[5]; bytes.6 = ptr[6]; bytes.7 = ptr[7]
+            bytes.8 = ptr[8]; bytes.9 = ptr[9]; bytes.10 = ptr[10]; bytes.11 = ptr[11]
+            bytes.12 = ptr[12]; bytes.13 = ptr[13]; bytes.14 = ptr[14]; bytes.15 = ptr[15]
+        }
+        let decodedUUID = UUID(uuid: bytes)
+        XCTAssertEqual(uuid, decodedUUID)
     }
 }
