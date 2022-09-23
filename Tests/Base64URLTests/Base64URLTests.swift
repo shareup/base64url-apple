574f70090fe4a4bfe317a55f66d880b9312d1992
Anthony Drendel <me@anthonydrendel.com>
2022-09-23 13:46:32 +0200

    Update to Swift 5.7

--- Tests/Base64URLTests/Base64URLTests.swift
+++ Tests/Base64URLTests/Base64URLTests.swift
@@ -16,14 +16,15 @@ final class Base64Tests: XCTestCase {
             "⦿": "4qa_",
         ]
 
-        try vectors.forEach { (expectedString, expectedEncodedString) throws in
+        try vectors.forEach { expectedString, expectedEncodedString throws in
             let expectedEncodedData = Data(expectedEncodedString.utf8)
             let expectedData = Data(expectedString.utf8)
 
             let encodedString = expectedData.base64URLEncodedString()
             let encodedData = expectedData.base64URLEncodedData()
 
-            let decodedDataFromString = try XCTUnwrap(Data(base64URLEncoded: expectedEncodedString))
+            let decodedDataFromString =
+                try XCTUnwrap(Data(base64URLEncoded: expectedEncodedString))
 
             let decodedDataFromData = try XCTUnwrap(Data(base64URLEncoded: expectedEncodedData))
 
@@ -49,7 +50,7 @@ final class Base64Tests: XCTestCase {
 
         let decoded = try XCTUnwrap(Data(base64URLEncoded: "Z_Jmr6iCSUm39lLh8-3AjQ"))
         var bytes: uuid_t = (0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
-        decoded.withUnsafeBytes { (ptr: UnsafeRawBufferPointer) -> Void in
+        decoded.withUnsafeBytes { (ptr: UnsafeRawBufferPointer) in
             bytes.0 = ptr[0]; bytes.1 = ptr[1]; bytes.2 = ptr[2]; bytes.3 = ptr[3]
             bytes.4 = ptr[4]; bytes.5 = ptr[5]; bytes.6 = ptr[6]; bytes.7 = ptr[7]
             bytes.8 = ptr[8]; bytes.9 = ptr[9]; bytes.10 = ptr[10]; bytes.11 = ptr[11]
